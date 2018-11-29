0a4785a1f95f340a74959a2b0300a5d04b87206e
kennethZha <kennethzha7@gmail.com>
2018-11-28 22:35:29 -0800

    updating netart

--- p5-project/p5-blankExample/sketch.js
+++ p5-project/p5-blankExample/sketch.js
@@ -1,85 +1,132 @@
 // declare variables here
+var cursorImage;
+var cross;
+var counter = 0;
+var timeleft = 0;
+
+ function preload () {
+   cross = loadImage ('../../cursor.png');
+ }
+
+ function convertSeconds (s) {
+   var min = floor(s / 60);
+   var sec = s % 60;
+   return nf(min,2) + ':' + nf(sec,2);
+ }
 
 function setup() {
   // put setup code here --> this runs once upon launch
   createCanvas(windowWidth, windowHeight);
-  background(255);
-  textFont("Courier");
+  text('Courier');
+  //noCursor();
 
+  //timer
+  var timer = select('#timer');
+  timer.html(convertSeconds(timeleft + counter));
+
+  function timeIt() {
+    counter ++;
+    timer.html(convertSeconds(timeleft + counter));
+  }
+
+  setInterval(timeIt, 1000);
 }
 
 function draw() {
   // put drawing code here --> this loops every frame
+  if (mouseX > windowWidth - 150 && mouseY < 100) {
+      cursor (cross);
+    }
+}
+
+function mouseOver() {
+  if (mouseX > windowWidth - 150 && mouseY < 100) {
+    fill(0);
+    rect(windowWidth - 200, 50,50,20);
+    //fill(95,158,160);
+    text('refresh?',windowWidth - 200, 50);
+  }
+}
+
+// refreshing page function
+function mouseClicked() {
+  if (mouseX > windowWidth - 150 && mouseY < 80) {
+          window.open("index.html","_self")
+  }
+  if (mouseX < 200 && mouseY < 80) {
+      //addTime.html(timer + 20,true);
+      color(255);
+      text("hi", 100,100);
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-setTimeout(arg1,6000);
+hamster = setTimeout(arg1,6000);
 
 function arg1() {
-  background(255);
-  fill(96);
+  fill(255);
   noStroke();
-  textSize(15);
-  text("5 %", 730, 100);
-  rect(50,110,90,5);
+  textSize(14);
+  text("- hamsters hard at work -", 640, 730);
+  rect(40,750,90,5);
 }
 
-setTimeout(arg2,9000);
+pizza = setTimeout(arg2,9000);
 
 function arg2() {
-  background(255);
-  fill(96);
+  clear();
+  fill(255);
   noStroke();
-  textSize(15);
-  text("13 %", 730, 100);
-  rect(50,110,230,5);
+  textSize(14);
+  text("- eating pizza -", 680, 730);
+  rect(40,750,230,5);
 }
 
 setTimeout(arg3,20000);
 
 function arg3() {
-  background(255);
-  fill(96);
+  clear();
+  fill(255);
   noStroke();
-  textSize(15);
-  text("27 %", 730, 100);
-  rect(50,110,460,5);
+  textSize(14);
+  text("- taking a shit -", 680, 730);
+  rect(40,750,460,5);
 }
 
 setTimeout(arg4,40000);
 
 function arg4() {
-  background(255);
-  fill(96);
+  clear();
+  fill(255);
   noStroke();
-  textSize(15);
-  text("45 %", 730, 100);
-  rect(50,110,670,5);
+  textSize(14);
+  text("- procrastinating -", 680, 730);
+  rect(40,750,670,5);
 }
 
 setTimeout(arg5,60000);
 
 function arg5() {
-  background(255);
-  fill(96);
+  clear();
+  fill(255);
   noStroke();
-  textSize(15);
-  text("63 %", 730, 100);
-  rect(50,110,850,5);
+  textSize(14);
+  text("- sleeping -", 680, 730);
+  rect(40,750,850,5);
 }
 
 setTimeout(arg6,80000);
 
 function arg6() {
-  background(255);
-  fill(96);
+  clear();
+  fill(255);
   noStroke();
-  textSize(15);
-  text("99 %", 730, 100);
-  rect(50,110,1345,5);
+  textSize(14);
+  text("- hamsters working overtime -", 640, 730);
+  rect(40,750,1345,5);
 }
 
 setTimeout(timeover,120000);
--- p5-project/p5-blankExample/sketchbow.js
+++ p5-project/p5-blankExample/sketchbow.js
@@ -1,6 +1,7 @@
 // declare variables here
 var done = false;
 var ellipseWidth;
+var shape;
 
 function setup() {
   // put setup code here --> this runs once upon launch
@@ -13,12 +14,13 @@ function draw() {
   // put drawing code here --> this loops every frame
   if(done == true){
     strokeWeight(60);
-    stroke(173, 224,255);
+    stroke(95,158,160);
     noFill();
-    ellipse(width/2.05, height/1.6, ellipseWidth);
-    ellipseWidth -= 10;
-    if(ellipseWidth <= 0){
-      window.open("../../index.html","_self")
+    ellipse(width/2.00, height/1.9, ellipseWidth);
+    ellipseWidth -= (10,30);
+    if(ellipseWidth <= 600){
+      //window.open("../../Art75.html","_self")
+      exit();
     }
   }
 }
@@ -27,11 +29,51 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-setTimeout(timeover,5900);
+setTimeout(timeover,500);
 
 function timeover() {
   done = true;
 }
 
+setTimeout(arg1,2200);
 
+function arg1() {
+  textSize(23);
+  fill(255);
+  noStroke();
+  text('Thank you for your patients',570,70);
+}
+
+setTimeout (arg2,2200);
+
+function arg2() {
+  noFill();
+  stroke(255);
+  strokeWeight(8);
+  ellipse(736,417,600,600);
+}
+
+setTimeout (arg3,6000);
+
+function arg3() {
+  textSize(17);
+  fill(255);
+  noStroke();
+  text('back',360,400);
+  fill(255);
+  triangle(320,395,340,380,340,410);
+}
+
+function mouseClicked() {
+  if (mouseX > 320 && mouseX <400 && mouseY > 380 && mouseY < 410 ) {
+    window.open("index.html","_self")
+  }
+}
+
+setTimeout(arg4,120000);
+
+function arg4() {
+  window.open("index.html","_self")
+
+}
 // write custom functions here
